997478540300c513aea7317aecebf61ed6a3ecf0
Michalk2003-pl <mkulikowski2003@pm.me>
2026-03-25 23:09:47 +0100

    added collection config

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection } from "astro:content";
+import { file } from "astro/loaders";
+import { z } from "astro/zod";
+
+const changeLog = defineCollection({
+  loader: file("src/content/changeLog/changeLog.jsonc"),
+  schema: z.object({
+    id: z.number(),
+    year: z.number(),
+    date: z.string(),
+    change: z.string(),
+  }),
+});
+
+export const collections = { changeLog };
